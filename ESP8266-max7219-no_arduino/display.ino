a8f0a549fc76b2ba1689cfe8556428ebd01fc682
MTS Standish <16389869+mattThurstan@users.noreply.github.com>
2020-04-21 21:21:10 +0100

    v0.2 - Converting to MD_MAX72XX library

--- ESP8266-max7219-no_arduino/display.ino
+++ ESP8266-max7219-no_arduino/display.ino
@@ -9,69 +9,38 @@
  *  
  *  Delay=bad programming.
  */
-void displayText ( char * theText)
+void displayText ( const char *theText)
 {
-  if ( myTime + ANIM_DELAY < millis()) 
-  {
-    myTime=millis();
+  uint8_t charWidth;
+  uint8_t cBuf[8];  // this should be ok for all built-in fonts
+  mx.clear();
+  
+//  if ( myTime + ANIM_DELAY < millis()) 
+//  {
+//    myTime=millis();
     // Draw the text to the current position
-    drawString(theText, len, x, 0);
+    //drawString(theText, len, x, 0);
     // In case you wonder why we don't have to call lmd.clear() in every loop: The font has a opaque (black) background... 
     // Toggle display of the new framebuffer
-    lmd.display();
+    //lmd.display();
     // Advance to next coordinate
-    if( --x < len * -8 )
+    //if( --x < len * -8 )
+    //{
+    //  x = LEDMATRIX_WIDTH;
+    //  lmd.clear();
+    //}
+    while (*theText != '\0')
     {
-      x = LEDMATRIX_WIDTH;
-      lmd.clear();
+      charWidth = mx.getChar(*theText++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
+    
+      for (uint8_t i=0; i<=charWidth; i++)  // allow space between characters
+      {
+        mx.transform(MD_MAX72XX::TSL);
+        if (i < charWidth)
+          mx.setColumn(0, cBuf[i]);
+        //delay(DELAYTIME);
+        delay(ANIM_DELAY);
+      }
     }
-  }
-}
-
-/**
- * This function draws a string of the given length to the given position.
- */
-void drawString(char* text, int len, int x, int y )
-{
-  for( int idx = 0; idx < len; idx ++ )
-  {
-    int c = text[idx] - 32;
-
-    // stop if char is outside visible area
-    if( x + idx * 8  > LEDMATRIX_WIDTH ) 
-    {
-      return;
-    }
-
-    // only draw if char is visible
-    if( 8 + x + idx * 8 > 0 ) 
-    {
-      drawSprite( font[c], x + idx * 8, y, 8, 8 );
-    }
-  }
-}
-
-/**
- * This draws a sprite to the given position using the width and height supplied (usually 8x8)
- */
-void drawSprite( byte* sprite, int x, int y, int width, int height )
-{
-  // The mask is used to get the column bit from the sprite row
-  byte mask = B10000000;
-
-  for( int iy = 0; iy < height; iy++ )
-  {
-    for( int ix = 0; ix < width; ix++ )
-    {
-      //Yes my font is backwards so I swap it around.
-      //lmd.setPixel(x + ix, y + iy, (bool)(sprite[iy] & mask ));
-      lmd.setPixel(x + (width - ix), y + iy, (bool)(sprite[iy] & mask ));
-
-      // shift the mask by one pixel to the right
-      mask = mask >> 1;
-    }
-
-    // reset column mask
-    mask = B10000000;
-  }
+//  }
 }
