51b6a0fba37804907b0cef65a6d9178d9924eb54
MTS Standish <16389869+mattThurstan@users.noreply.github.com>
2020-04-22 04:57:44 +0100

    v0.4 - Added DS3231 Clock

--- Main/Board/ClockMessageBox/ClockMessageBox.ino
+++ Main/Board/ClockMessageBox/ClockMessageBox.ino
@@ -3,6 +3,7 @@
  * 
  * Wemos D1 Mini Pro (ESP8266)
  * Max7219 LED matrix
+ * DS3231 Clock
  * 
  * Original project by aapicella.
  * https://github.com/aapicella/WiFi-enables-LED-Matrix
@@ -12,11 +13,28 @@
 #include <WiFiManager.h>                  // https://github.com/tzapu/WiFiManager - web page @ 192.168.4.1
 #include <SPI.h>
 #include <MD_MAX72xx.h>
+/* 
+ *  _BV(bit) is a macro defined in  avr/io.h file in AVR libraries.
+ *  Added the following to DS3231_Simple.h for temporary fix.
+ *  
+ * #define _BV(bit) \
+ * (1 << (bit))
+ */
+#include <DS3231_Simple.h>
 
 /*----------------------------system----------------------------*/
 const String _progName = "ClockMessageBox";
-const String _progVers = "0.3";           // Added tzapu WIFI Manager
+const String _progVers = "0.4";           // Added DS3231 Clock
 #define DEBUG 1                           // 0 or 1 - remove later
+
+/*----------------------------pins------------------------------*/
+// Max7219 to Wemos D1 Mini Pro (SPI - Serial) - 5V
+#define DATA_PIN  13                      // DIN to D7 (MOSI - GPIO 13)
+#define CS_PIN    12                      // CS  to D6 (MISO - GPIO 12)
+#define CLK_PIN   14                      // CLK to D5 (SCLK - GPIO 14)
+// DS3231 Clock to Wemos D1 Mini Pro (I2C) - 5V
+#define SCL_PIN   5                       // SCL to D1 (GPIO5)
+#define SDA_PIN   4                       // SDA to D2 (GPIO4)
 
 /*-----------------------------WIFI-----------------------------*/
 WiFiServer _server(80);                   // TCP server at port 80 will respond to HTTP requests
@@ -28,15 +46,6 @@ const char* _apName = "ClockMessageBox";
 const char* _apPassword = "password";
 
 /*----------------------------LED Matrix------------------------*/
-/* Max7219 to Wemos D1 Mini Pro
- * DIN to D7 (GPIO 13)
- * CS to D6 (GPIO 12)
- * CLK to D5 (GPIO 14)
- */
-#define DATA_PIN  13                      // DIN to D7 (GPIO 13)
-#define CS_PIN    12                      // CS to D6 (GPIO 12)
-#define CLK_PIN   14                      // CLK to D5 (GPIO 14)
-
 #define HARDWARE_TYPE MD_MAX72XX::GENERIC_HW  // PAROLA_HW
 #define MAX_DEVICES 2
 MD_MAX72XX _mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
@@ -46,6 +55,9 @@ const int LEDMATRIX_WIDTH = 31;
 //const int LEDMATRIX_HEIGHT = 7;
 //const int LEDMATRIX_SEGMENTS = 4;
 int x = LEDMATRIX_WIDTH, y=0;             // start top left
+
+/*----------------------------DS3231----------------------------*/
+ DS3231_Simple Clock;
 
 /*----------------------------values----------------------------*/
 char _text[75] = " Hello ";               // Marquee text
@@ -60,7 +72,8 @@ void setup()
     Serial.begin(115200);
     Serial.println();
   }
-
+  
+  Clock.begin();
   _mx.begin();                            // Initialize the display
   displayText(_text);
   
@@ -83,6 +96,13 @@ void setup()
 void loop() 
 {
   displayText(_text);
+
+  if (DEBUG) {
+    Clock.printDateTo_YMD(Serial);
+    Serial.print(' ');
+    Clock.printTimeTo_HMS(Serial);
+    Serial.println();
+  }
 
   if (_wifiAvailable) {
     webMessage();
--- README.md
+++ README.md
@@ -4,11 +4,13 @@ LED Matrix and ESP8266 project.
 Hardware used:
 - Wemos D1 Mini Pro (ESP8266)
 - Max7219 LED matrix
+- DS3231 Clock
 
 Libraries used:
 - https://github.com/esp8266/Arduino
 - https://github.com/MajicDesigns/MD_MAX72XX
 - https://github.com/tzapu/WiFiManager
+- https://github.com/sleemanj/DS3231_Simple
 
 Originally based on project by aapicella.
 - https://github.com/aapicella/WiFi-enables-LED-Matrix
